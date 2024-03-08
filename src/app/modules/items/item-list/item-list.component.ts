00f8ac2239400c5157b7ac0fbe6360b921918cfe
Nicolas Kobel <nicolas.kobel@hes-so.ch>
2024-03-08 08:55:49 +0100

    start branch

--- src/app/modules/items/item-list/item-list.component.ts
+++ src/app/modules/items/item-list/item-list.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromStore from '../../../store';
+import { Observable, of } from 'rxjs';
+import { Item } from '../../../shared/models/item.model';
 
 @Component({
   selector: 'app-item-list',
@@ -6,5 +11,10 @@ import { Component } from '@angular/core';
   styleUrl: './item-list.component.css'
 })
 export class ItemListComponent {
+  items$: Observable<Item[]> = of([])
 
+  constructor(private store:Store<fromStore.fromReducers.EscapeRoomState>){
+    
+  }
+  
 }
